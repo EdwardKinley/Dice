75b0177d830e89792a686aa3e7592f4bad67c2ec
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-07-15 22:06:51 +0100

    determines next square and improves visual arrangement of dice quantity options

--- js/dice.js
+++ js/dice.js
@@ -2,17 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const main = document.querySelector('.main');
 
-  const maxDiceQuantity = 16;
+  const maxDiceQuantity = 9;
   const diceQuantityOptions = [];
+  nextSquare = 0;
 
   // const diceQuantityOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   addDiceQuantityOptionsToArray();
+  determineNextSquare();
   showDiceQuantityOptions();
 
   function addDiceQuantityOptionsToArray() {
     for (i=0; i<maxDiceQuantity; i++) {
       diceQuantityOptions.push(i+1);
+    }
+  }
+
+  function determineNextSquare() {
+    j=maxDiceQuantity;
+    while (nextSquare == 0) {
+      // console.log(j);
+      // console.log(Math.sqrt(j));
+      if (Number.isInteger(Math.sqrt(j))) {
+        nextSquare = j;
+      } else {
+        j++;
+      }
     }
   }
 
@@ -38,16 +53,15 @@ document.addEventListener('DOMContentLoaded', () => {
   function showDiceQuantityNumbers() {
     const numbersSpace = document.querySelector('.numbersSpace');
     for (i=0; i<diceQuantityOptions.length; i++) {
-      // console.log(diceQuantityOptions[i]);
       const quantity = document.createElement('div');
       quantity.className = 'quantity';
       quantity.textContent = diceQuantityOptions[i];
-      quantity.style.height = `${94/(diceQuantityOptions.length**0.5)}%`;
-      quantity.style.width = `${94/(diceQuantityOptions.length**0.5)}%`;
-      quantity.style.minWidth = `${94/(diceQuantityOptions.length**0.5)}%`;
-      quantity.style.margin = `${3/(diceQuantityOptions.length**0.5)}%`;
+      quantity.style.height = `${94/(nextSquare**0.5)}%`;
+      quantity.style.width = `${94/(nextSquare**0.5)}%`;
+      quantity.style.minWidth = `${94/(nextSquare**0.5)}%`;
+      quantity.style.margin = `${3/(nextSquare**0.5)}%`;
       quantity.style.fontSize = `${45/(diceQuantityOptions.length**0.5)}vh`;
-      quantity.style.lineHeight = `${88/(diceQuantityOptions.length**0.5)}vh`;
+      quantity.style.lineHeight = `${88/(nextSquare**0.5)}vh`;
       numbersSpace.appendChild(quantity);
       quantity.addEventListener('click', () => {
         // console.log(parseInt(quantity.textContent));
@@ -57,6 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function rollDice(n) {
+    main.innerHTML = '';
     for (i=0; i<n; i++) {
       console.log('roll', i+1);
     }
